e78717660689cc5c00c1a1de290208d56b37ad8e
Harisankar Krishnadas <harisankar.spab@gmail.com>
2023-04-05 15:05:55 -0500

    Water analysis

--- Africa water crisis.py
+++ Africa water crisis.py
@@ -6,11 +6,13 @@ Created on Fri Mar 31 23:42:29 2023
 @author: harisankar
 """
 
-
 from pandas_datareader import wb
 import pandas as pd
 import awoc
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 indicator = ['NY.GDP.PCAP.PP.CD', 'SI.POV.GINI', 'SE.XPD.TOTL.GD.ZS', 
              'SE.PRM.CMPT.FE.ZS','SE.PRM.CMPT.MA.ZS', 'EG.ELC.ACCS.ZS',
@@ -56,3 +58,85 @@ df_wb = df_wb.merge(df_country,on='Country', how='inner') # merging columns
 df_wb['Country'].nunique() # number of countries
 
 
+# Exploring data
+#df_wb.head
+#list(df_wb.columns)
+
+
+
+# Basic histogram
+plt.hist(df_wb['% Annual Freshwater Withdrawals (internal)'], bins=10)
+plt.title('Annual Freshwater Withdrawals in sub-Saharan Africa')
+plt.xlabel('% Annual Freshwater Withdrawals (internal)')
+plt.ylabel('Frequency')
+
+
+# Basic boxplot across years year
+df_bp = df_wb[df_wb['% Annual Freshwater Withdrawals (internal)'].notnull()]
+plt.boxplot(df_bp['% Annual Freshwater Withdrawals (internal)'].groupby(df_bp['Year']).apply(list), labels=df_bp['Year'].unique())
+plt.title('Quartiles of Annual Freshwater Withdrawals')
+plt.xlabel('Year')
+plt.ylabel('Freshwater Withdrawals (% of total renewable water resources)')
+
+
+# Boxplot by country
+df_wb_water = df_wb[df_wb['% Annual Freshwater Withdrawals (internal)'].notna()]
+grouped_data = df_wb_water.groupby('Country')['% Annual Freshwater Withdrawals (internal)'].apply(list)
+plt.boxplot(grouped_data.values)
+plt.title('Quartiles of Annual Freshwater Withdrawals')
+plt.xlabel('Country')
+plt.ylabel('Freshwater Withdrawals (% of total renewable water resources)')
+
+
+# Average % withdrawal for all years across countries
+df_drop = df_wb.dropna(subset=['% Annual Freshwater Withdrawals (internal)'])
+grouped_data = df_drop.groupby('Country')['% Annual Freshwater Withdrawals (internal)'].mean()
+grouped_data = grouped_data.sort_values(ascending=False) #sort in descending order
+plt.bar(grouped_data.index, grouped_data.values)
+plt.title('Mean Freshwater Withdrawal by Country')
+plt.xlabel('Country')
+plt.ylabel('Freshwater Withdrawal (billion cubic meters)')
+plt.xticks(rotation=90)
+plt.show()
+
+
+# Plot quartiles 
+df_drop = df_wb.dropna(subset=['% Annual Freshwater Withdrawals (internal)'])
+
+#create a new column with quartile information
+df_drop['Quartile'] = pd.qcut(df_drop['% Annual Freshwater Withdrawals (internal)'], q=4, labels=['Q1', 'Q2', 'Q3', 'Q4'])
+
+#group the data by country and quartile
+grouped_data = df_drop.groupby(['Country', 'Quartile'])['% Annual Freshwater Withdrawals (internal)'].mean().reset_index()
+
+# Create subplots for each quartile
+g = sns.FacetGrid(grouped_data, col='Quartile', col_wrap=2, height=4)
+g = g.map(sns.barplot, 'Country', '% Annual Freshwater Withdrawals (internal)')
+g.set_xticklabels(rotation=90)
+g.fig.subplots_adjust(top=0.9)
+g.fig.suptitle('Mean Freshwater Withdrawal by Country Quartile')
+
+
+
+#Plot quartiles separately 
+df_drop = df_wb.dropna(subset=['% Annual Freshwater Withdrawals (internal)'])
+
+#create a new column with quartile information
+df_drop = df_drop.sort_values('% Annual Freshwater Withdrawals (internal)')
+df_drop['Quartile'] = pd.qcut(df_drop['% Annual Freshwater Withdrawals (internal)'], q=4, labels=['Q1', 'Q2', 'Q3', 'Q4'])
+
+#group the data by country and quartile
+grouped_data = df_drop.groupby(['Country', 'Quartile'])['% Annual Freshwater Withdrawals (internal)'].mean().reset_index()
+grouped_data.dropna(subset=['% Annual Freshwater Withdrawals (internal)'], inplace=True)
+grouped_data = grouped_data.sort_values('% Annual Freshwater Withdrawals (internal)')
+
+
+quartiles = ['Q1', 'Q2', 'Q3', 'Q4']
+for quartile in quartiles:
+    subset = grouped_data[grouped_data['Quartile'] == quartile]
+    plt.figure()
+    ax = sns.barplot(x='Country', y='% Annual Freshwater Withdrawals (internal)', data=subset)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+    ax.set_title('Mean Freshwater Withdrawal by Country - ' + quartile)
+    ax.set_xlabel('Country')
+    ax.set_ylabel('% Annual Freshwater Withdrawals (internal)')
